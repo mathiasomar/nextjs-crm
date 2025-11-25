62290b0b088f33452f31c32d0150e9c49f14ae43
mathiasomar <mathiasomar@gmail.com>
2025-11-25 18:13:34 +0300

    Get all contacts endpoint

--- backend/src/controllers/contacts.controller.ts
+++ backend/src/controllers/contacts.controller.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
+import { prisma } from "../../lib/prisma";
 
 export const getContacts = async (req: Request, res: Response) => {
-  // Query params: search, tag, owner, page
+  // Query params: search:q, tag, orgId(organization scope), ownerId, page, limit
+  const { q, tag, orgId, ownerId } = req.query;
+  const contacts = await prisma.contact.findMany({
+    where: {
+      OR: [
+        { firstName: { contains: q as string, mode: "insensitive" } },
+        { lastName: { contains: q as string, mode: "insensitive" } },
+        { email: { contains: q as string, mode: "insensitive" } },
+      ],
+    },
+  });
+
+  return res.status(200).json(contacts);
 };
 export const createContact = async (req: Request, res: Response) => {};
 export const getContact = async (req: Request, res: Response) => {};
