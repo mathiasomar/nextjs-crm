919c1c72cc100901a39b840e45df190277796535
mathiasomar <ommykmathy@gmail.com>
2025-12-03 12:57:09 +0300

    Remove clerk authentication from frontend

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import QueryProvider from "@/components/providers/query-provider";
-import { auth } from "@clerk/nextjs/server";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +25,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { getToken } = await auth();
-  const token = await getToken();
-  console.log(token);
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
